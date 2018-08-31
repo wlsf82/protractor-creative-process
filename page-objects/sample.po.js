ee6e577a507aa84928c0bca047b15f30282a5826
Walmyr <wlsf82@gmail.com>
2018-08-31 16:32:06 +0200

    Initial implementation of test components
    The tests were left failing for teaching purposes.
    In another session the test component will be implemented to fix
    the tests and the page object.
    
    Also, the page object was renamed using uppercase in the first
    letter since this is a class.

--- page-objects/sample.po.js
+++ page-objects/sample.po.js
@@ -1,20 +1,32 @@
 const helper = require('protractor-helper');
 
+const InteractionsFromBottom = require('../components/InteractionsFromBottom.js');
+const InteractionsFromTop = require('../components/InteractionsFromTop.js');
+const Loading = require('../components/Loading.js');
+const Table = require('../components/Table.js');
+
 class Page {
     constructor() {
-        this.tableElements = element.all(by.css('.table .table-row'));
-        this.moreButton = element(by.css('.interactions button[type="button"]'));
-        this.loadingElement = element(by.className('loading'));
-        this.searchInputTextField = element(by.css('.interactions input[type="text"]'));
-        this.searchButton = element(by.css('.interactions button[type="submit"]'));
-        this.dismissButtonOfFirstItem = element.all(by.css('.table-row .button-inline')).first();
+        this.relativeUrl = '';
+
+        this.interactionsFromBottom = new InteractionsFromBottom();
+        this.interactionsFromTop = new InteractionsFromTop();
+        this.loading = new Loading();
+        this.table = new Table();
+
+        // this.tableElements = element.all(by.css('.table .table-row'));
+        // this.moreButton = element(by.css('.interactions button[type="button"]'));
+        // this.loadingElement = element(by.className('loading'));
+        // this.searchInputTextField = element(by.css('.interactions input[type="text"]'));
+        // this.searchButton = element(by.css('.interactions button[type="submit"]'));
+        // this.dismissButtonOfFirstItem = element.all(by.css('.table-row .button-inline')).first();
     }
 
-    searchForTermAfterClearingInputField(term) {
-        helper.clearFieldWhenVisible(this.searchInputTextField);
-        helper.fillFieldWithTextWhenVisible(this.searchInputTextField, term)
-        helper.clickWhenClickable(this.searchButton);
-    }
+    // searchForTermAfterClearingInputField(term) {
+    //     helper.clearFieldWhenVisible(this.searchInputTextField);
+    //     helper.fillFieldWithTextWhenVisible(this.searchInputTextField, term)
+    //     helper.clickWhenClickable(this.searchButton);
+    // }
 }
 
 module.exports = Page;
--- specs/sample.spec.js
+++ specs/sample.spec.js
@@ -1,67 +1,67 @@
 const helper = require('protractor-helper');
 
-const Page = require('../page-objects/sample.po.js');
+const Page = require('../page-objects/Sample.po.js');
 
 describe('Hackernews kind off app', () => {
   const page = new Page();
 
-  beforeEach(() => browser.get(''));
+  beforeEach(() => browser.get(page.relativeUrl));
 
   // afterEach(() => browser.sleep(1000));
 
   it('renders 100 items in the first visit', () => {
-    helper.waitForElementVisibility(page.tableElements.first());
-    helper.waitForElementVisibility(page.tableElements.last());
+    helper.waitForElementVisibility(page.table.items.first());
+    helper.waitForElementVisibility(page.table.items.last());
 
-    expect(page.tableElements.count()).toBe(100);
+    expect(page.table.items.count()).toBe(100);
   });
 
   it('quickly shows a loading component when clicking the More button', () => {
-    helper.clickWhenClickable(page.moreButton);
+    helper.clickWhenClickable(page.interactionsFromBottom.moreButton);
 
-    helper.waitForElementVisibility(page.loadingElement);
+    helper.waitForElementVisibility(page.loading.ellipsis);
   });
 
   it('renders 200 items after clicking the More button', () => {
-    helper.clickWhenClickable(page.moreButton);
+    helper.clickWhenClickable(page.interactionsFromBottom.moreButton);
 
-    helper.waitForElementVisibility(page.loadingElement);
-    helper.waitForElementNotToBePresent(page.loadingElement);
-    helper.waitForElementVisibility(page.tableElements.first());
-    helper.waitForElementVisibility(page.tableElements.last());
+    helper.waitForElementVisibility(page.loading.ellipsis);
+    helper.waitForElementNotToBePresent(page.loading.ellipsis);
+    helper.waitForElementVisibility(page.table.items.first());
+    helper.waitForElementVisibility(page.table.items.last());
 
-    expect(page.tableElements.count()).toBe(200);
+    expect(page.table.items.count()).toBe(200);
   });
 
   it('quickly shows a loading component when searching for the word "react" for the first time', () => {
-    page.searchForTermAfterClearingInputField('react');
+    page.interactionsFromTop.searchForTermAfterClearingInputField('react');
 
-    helper.waitForElementVisibility(page.loadingElement);
+    helper.waitForElementVisibility(page.loading.ellipsis);
   });
 
   it('renders 100 items after searching for the word "react" for the first time', () => {
-    page.searchForTermAfterClearingInputField('react');
+    page.interactionsFromTop.searchForTermAfterClearingInputField('react');
 
-    helper.waitForElementVisibility(page.loadingElement);
-    helper.waitForElementNotToBePresent(page.loadingElement);
+    helper.waitForElementVisibility(page.loading.ellipsis);
+    helper.waitForElementNotToBePresent(page.loading.ellipsis);
 
-    expect(page.tableElements.count()).toBe(100)
+    expect(page.table.items.count()).toBe(100);
   });
 
   it('does not renders a loading component after searching for "react" and then "redux" again', () => {
-    page.searchForTermAfterClearingInputField('react');
+    page.interactionsFromTop.searchForTermAfterClearingInputField('react');
 
-    helper.waitForElementVisibility(page.loadingElement);
-    helper.waitForElementNotToBePresent(page.loadingElement);
+    helper.waitForElementVisibility(page.loading.ellipsis);
+    helper.waitForElementNotToBePresent(page.loading.ellipsis);
 
-    page.searchForTermAfterClearingInputField('redux');
+    page.interactionsFromTop.searchForTermAfterClearingInputField('redux');
 
-    helper.waitForElementNotToBePresent(page.loadingElement);
+    helper.waitForElementNotToBePresent(page.loading.ellipsis);
   });
 
   it('shows only 99 items after dismissing one item', () => {
-    helper.clickWhenClickable(page.dismissButtonOfFirstItem)
+    helper.clickWhenClickable(page.table.dismissButtonOfFirstItem);
 
-    expect(page.tableElements.count()).toBe(99);
+    expect(page.table.items.count()).toBe(99);
   });
 });
