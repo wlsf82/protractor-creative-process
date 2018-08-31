14f3ea982827ee0510c75ddf7849c8ef1c09fba6
Walmyr <wlsf82@gmail.com>
2018-08-31 15:21:40 +0200

    Initial implementation of the page objects pattern
    These implementation is leaving the code broken on purpose due to
    teaching reasons.
    The idea is that in this lesson we will start thinking about page
    objects without really having them, and in the next session the
    real page object will be implemented to fix the tests.

--- specs/sample.spec.js
+++ specs/sample.spec.js
@@ -1,6 +1,9 @@
 const helper = require('protractor-helper');
 
+const Page = require('../page-objects/sample.po.js');
+
 describe('Hackernews kind off app', () => {
+  const page = new Page();
   // Test's pre-condition
   beforeEach(() => browser.get(''));
 
@@ -8,122 +11,126 @@ describe('Hackernews kind off app', () => {
 
   it('renders 100 items in the first visit', () => {
     // Element definition
-    const tableElements = element.all(by.css('.table .table-row'));
+    // const tableElements = element.all(by.css('.table .table-row'));
 
     // Wait for elements before assertion
-    helper.waitForElementVisibility(tableElements.first());
-    helper.waitForElementVisibility(tableElements.last());
+    helper.waitForElementVisibility(page.tableElements.first());
+    helper.waitForElementVisibility(page.tableElements.last());
 
     // Assertion
-    expect(tableElements.count()).toBe(100);
+    expect(page.tableElements.count()).toBe(100);
   });
 
   it('quickly shows a loading component when clicking the More button', () => {
     // Elements definition
-    const moreButton = element(by.css('.interactions button[type="button"]'));
-    const loadingElement = element(by.className('loading'));
+    // const moreButton = element(by.css('.interactions button[type="button"]'));
+    // const loadingElement = element(by.className('loading'));
 
     // Clicks in the element when it is clickable
-    helper.clickWhenClickable(moreButton);
+    helper.clickWhenClickable(page.moreButton);
 
     // Wait for element as assertion
-    helper.waitForElementVisibility(loadingElement);
+    helper.waitForElementVisibility(page.loadingElement);
   });
 
   it('renders 200 items after clicking the More button', () => {
     // Elements definition
-    const moreButton = element(by.css('.interactions button[type="button"]'));
-    const loadingElement = element(by.className('loading'));
+    // const moreButton = element(by.css('.interactions button[type="button"]'));
+    // const loadingElement = element(by.className('loading'));
 
     // Clicks in the element when it is clickable
-    helper.clickWhenClickable(moreButton);
+    helper.clickWhenClickable(page.moreButton);
 
     // Wait for element to change state before defining the element that will be used on assertion
-    helper.waitForElementVisibility(loadingElement);
-    helper.waitForElementNotToBePresent(loadingElement);
+    helper.waitForElementVisibility(page.loadingElement);
+    helper.waitForElementNotToBePresent(page.loadingElement);
 
     // Element definition
-    const tableElements = element.all(by.css('.table .table-row'));
+    // const tableElements = element.all(by.css('.table .table-row'));
 
     // Wait for elements before assertion
-    helper.waitForElementVisibility(tableElements.first());
-    helper.waitForElementVisibility(tableElements.last());
+    helper.waitForElementVisibility(page.tableElements.first());
+    helper.waitForElementVisibility(page.tableElements.last());
 
     // Assertion
-    expect(tableElements.count()).toBe(200);
+    expect(page.tableElements.count()).toBe(200);
   });
 
   it('quickly shows a loading component when searching for the word "react" for the first time', () => {
     // Elements definition
-    const searchInputTextField = element(by.css('.interactions input[type="text"]'));
-    const searchButton = element(by.css('.interactions button[type="submit"]'))
-    const loadingElement = element(by.className('loading'));
+    // const searchInputTextField = element(by.css('.interactions input[type="text"]'));
+    // const searchButton = element(by.css('.interactions button[type="submit"]'))
+    // const loadingElement = element(by.className('loading'));
 
     // Actions
-    helper.clearFieldWhenVisible(searchInputTextField);
-    helper.fillFieldWithTextWhenVisible(searchInputTextField, 'react');
-    helper.clickWhenClickable(searchButton);
+    page.searchForTermAfterClearingInputField('react');
+    // helper.clearFieldWhenVisible(searchInputTextField);
+    // helper.fillFieldWithTextWhenVisible(searchInputTextField, 'react');
+    // helper.clickWhenClickable(searchButton);
 
     // Wait for element as assertion
-    helper.waitForElementVisibility(loadingElement);
+    helper.waitForElementVisibility(page.loadingElement);
   });
 
   it('renders 100 items after searching for the word "react" for the first time', () => {
     // Elements definition
-    const searchInputTextField = element(by.css('.interactions input[type="text"]'));
-    const searchButton = element(by.css('.interactions button[type="submit"]'))
-    const loadingElement = element(by.className('loading'));
+    // const searchInputTextField = element(by.css('.interactions input[type="text"]'));
+    // const searchButton = element(by.css('.interactions button[type="submit"]'))
+    // const loadingElement = element(by.className('loading'));
 
     // Actions
-    helper.clearFieldWhenVisible(searchInputTextField);
-    helper.fillFieldWithTextWhenVisible(searchInputTextField, 'react');
-    helper.clickWhenClickable(searchButton);
+    page.searchForTermAfterClearingInputField('react');
+    // helper.clearFieldWhenVisible(searchInputTextField);
+    // helper.fillFieldWithTextWhenVisible(searchInputTextField, 'react');
+    // helper.clickWhenClickable(searchButton);
 
     // Wait for element to change state before defining the element that will be used on assertion
-    helper.waitForElementVisibility(loadingElement);
-    helper.waitForElementNotToBePresent(loadingElement);
+    helper.waitForElementVisibility(page.loadingElement);
+    helper.waitForElementNotToBePresent(page.loadingElement);
 
     // Element definition
-    const tableElements = element.all(by.css('.table .table-row'));
+    // const tableElements = element.all(by.css('.table .table-row'));
 
     // Assertion
-    expect(tableElements.count()).toBe(100)
+    expect(page.tableElements.count()).toBe(100)
   });
 
   it('does not renders a loading component after searching for "react" and then "redux" again', () => {
     // Elements definition
-    const searchInputTextField = element(by.css('.interactions input[type="text"]'));
-    const searchButton = element(by.css('.interactions button[type="submit"]'))
-    const loadingElement = element(by.className('loading'));
+    // const searchInputTextField = element(by.css('.interactions input[type="text"]'));
+    // const searchButton = element(by.css('.interactions button[type="submit"]'))
+    // const loadingElement = element(by.className('loading'));
 
     // Actions
-    helper.clearFieldWhenVisible(searchInputTextField);
-    helper.fillFieldWithTextWhenVisible(searchInputTextField, 'react');
-    helper.clickWhenClickable(searchButton);
+    page.searchForTermAfterClearingInputField('react');
+    // helper.clearFieldWhenVisible(searchInputTextField);
+    // helper.fillFieldWithTextWhenVisible(searchInputTextField, 'react');
+    // helper.clickWhenClickable(searchButton);
 
     // Wait for element to change state before interacting with other elements.
-    helper.waitForElementVisibility(loadingElement);
-    helper.waitForElementNotToBePresent(loadingElement);
+    helper.waitForElementVisibility(page.loadingElement);
+    helper.waitForElementNotToBePresent(page.loadingElement);
 
-    helper.clearFieldWhenVisible(searchInputTextField);
-    helper.fillFieldWithTextWhenVisible(searchInputTextField, 'redux');
-    helper.clickWhenClickable(searchButton);
+    page.searchForTermAfterClearingInputField('redux');
+    // helper.clearFieldWhenVisible(searchInputTextField);
+    // helper.fillFieldWithTextWhenVisible(searchInputTextField, 'redux');
+    // helper.clickWhenClickable(searchButton);
 
     // Wait for element as assertion
-    helper.waitForElementNotToBePresent(loadingElement);
+    helper.waitForElementNotToBePresent(page.loadingElement);
   });
 
   it('shows only 99 items after dismissing one item', () => {
     // Element definition
-    const dismissButtonOfFirstItem = element.all(by.css('.table-row .button-inline')).first();
+    // const dismissButtonOfFirstItem = element.all(by.css('.table-row .button-inline')).first();
 
     // Action
-    helper.clickWhenClickable(dismissButtonOfFirstItem)
+    helper.clickWhenClickable(page.dismissButtonOfFirstItem)
 
     // Element definition
-    const tableElements = element.all(by.css('.table .table-row'));
+    // const tableElements = element.all(by.css('.table .table-row'));
 
     // Assertion
-    expect(tableElements.count()).toBe(99);
+    expect(page.tableElements.count()).toBe(99);
   });
 });
